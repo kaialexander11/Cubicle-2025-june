911f2ca2dbbb55d2ac275910e7b9d0998ae82928
Alexander Uzunov <kaialexander.11@gmail.com>
2025-06-05 19:41:31 +0300

    Add base cube model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 
-
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const dbConnect = require('./config/dbConfig');
@@ -21,9 +20,8 @@ dbConnect()
     .then(() => console.log('DB Connected successfully'))
     .catch(err => {
         console.log('DB error: ', err);
-    });
+});
 
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}... `));
-
