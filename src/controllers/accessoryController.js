82bbf72815ab26ec2f2bcc05193852b38d959364
Alexander Uzunov <kaialexander.11@gmail.com>
2025-06-06 09:31:09 +0300

    Add post action for create accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -4,4 +4,14 @@ router.get('/create', (req, res) => {
     res.render('accessory/create');
 });
 
+router.post('/create', (req, res) => {
+    
+    const body = req.body;
+
+    console.log(body);
+
+    res.redirect('/');
+
+});
+
 module.exports = router;
